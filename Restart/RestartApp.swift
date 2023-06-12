5b1d58c192dd542703bcb60d8a56add316a6ab17
petar.iliev <petar.iliev@servicenow.com>
2023-06-12 09:52:30 -0700

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Petar Iliev on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
